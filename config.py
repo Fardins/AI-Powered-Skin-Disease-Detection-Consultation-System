96c0fd6d72feae56a1c8d2104a40c7962a4ecab1
Md Atickur Rahman <66249317+Fardins@users.noreply.github.com>
2026-04-28 02:49:01 +0600

    configuration file

---
+++ config.py
@@ -0,0 +1,11 @@
+DATASET_PATH = "data/raw/IMG_CLASSES"
+OUTPUT_PATH = "data/processed"
+
+SPLIT_RATIO = 0.8
+
+MODEL_NAME = "yolo11n-cls.pt"
+TRAINED_MODEL_PATH = "runs/classify/train/weights/best.pt"
+
+IMG_SIZE = 224
+EPOCHS = 100
+BATCH_SIZE = 16
